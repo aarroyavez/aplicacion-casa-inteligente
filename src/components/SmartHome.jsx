cd13d15007d91331b6f50329f6de13aa0f7d627d
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-11 02:47:57 -0500

    Sin novedades

--- src/components/SmartHome.jsx
+++ src/components/SmartHome.jsx
@@ -1,19 +1,63 @@
+// import React from "react";
+// import {MainControls} from "./MainControls";
+// import {Light} from "./Light";
+// import { SmartHomeContext } from "../SmartHomeContext";
+
+// export function SmartHome() {
+//   const { lights } = React.useContext(SmartHomeContext);
+
+//   return (
+//     <div>
+//       <MainControls />
+//       {lights.map((light, index) => (
+//         <Light key={index} id={index} isOn={light} />
+//       ))}
+//     </div>
+//   );
+// }
+
+
+// import React from "react";
+// import { MainControls } from "./MainControls";
+// import { Light } from "./Light";
+// import { SmartHomeContext } from "../SmartHomeContext";
+
+// export function SmartHome() {
+//   const { lights } = React.useContext(SmartHomeContext);
+
+//   return (
+//     <div>
+//       <MainControls />
+//       {lights.map((isOn, index) => (
+//         <Light key={index} id={index} isOn={isOn} />
+//       ))}
+//     </div>
+//   );
+// }
+
 import React from "react";
-import {MainControls} from "./MainControls";
-import {Light} from "./Light";
+import { MainControls } from "./MainControls";
+import { Light } from "./Light";
 import { SmartHomeContext } from "../SmartHomeContext";
 
 export function SmartHome() {
   const { lights } = React.useContext(SmartHomeContext);
+  console.log(lights);
 
   return (
     <div>
       <MainControls />
-      {lights.map((light, index) => (
-        <Light key={index} id={index} isOn={light} />
+      {lights.map((isOn, index) => (
+        <Light key={index} id={index} isOn={isOn} />
       ))}
     </div>
   );
 }
+
+
+
+
+
+
 
 
