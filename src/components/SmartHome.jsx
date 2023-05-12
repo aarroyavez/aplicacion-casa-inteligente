1d2fd993b3e9dd162d2c4f05b025091cf5879440
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-05-12 11:24:58 -0500

    se ha culminado la práctica

--- src/components/SmartHome.jsx
+++ src/components/SmartHome.jsx
@@ -1,24 +1,4 @@
-// import React from "react";
-// import { MainControls } from "./MainControls";
-// import { Light } from "./Light";
-// import { SmartHomeContext } from "../SmartHomeContext";
-
-// export function SmartHome() {
-//   const { lights } = React.useContext(SmartHomeContext);
-//   console.log(lights);
-
-//   return (
-//     <div>
-//       <MainControls />
-//       {lights.map((isOn, index) => (
-//         <Light key={index} id={index} isOn={isOn} />
-//       ))}
-//     </div>
-//   );
-// }
-
 import React from "react";
-import { MainControls } from "./MainControls";
 import { Light } from "./Light";
 import { SmartHomeContext } from "../SmartHomeContext";
 
